e69dd6a7cdeb0599f22dcdf60630d218434a387b
ewan-mcjesus <saru.d.gr8@gmail.com>
2020-06-23 12:35:27 +0530

    stuff

--- marathon/lib/shared/constants.dart
+++ marathon/lib/shared/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 const textInputDecoration = InputDecoration(
-
   fillColor: Colors.white,
   filled: true,
   enabledBorder: OutlineInputBorder(
@@ -11,3 +10,21 @@ const textInputDecoration = InputDecoration(
       borderSide: BorderSide(color: Colors.pink,width: 2.0)
   ),
 );
+
+
+class ScreenConstants {
+  static double displayHeight;
+  static double displayWidth;
+  static double percentHeight;
+  static double percentWidth;
+  static MediaQueryData _mediaQueryData;
+
+  void init(BuildContext buildContext)
+  {
+    _mediaQueryData = MediaQuery.of(buildContext);
+    displayHeight =_mediaQueryData.size.height ;
+    displayWidth= _mediaQueryData.size.width;
+    percentHeight = displayHeight/100;
+    percentWidth = displayWidth/100;
+  }
+}
