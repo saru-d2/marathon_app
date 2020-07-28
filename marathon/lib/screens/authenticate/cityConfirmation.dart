83d50e4ff02779a0157880f6617257e7e97664ab
WafflesThe1st <abhisingam2@gmail.com>
2020-07-29 01:14:00 +0530

    fixed location services so that it returns the name of the city

--- marathon/lib/screens/authenticate/cityConfirmation.dart
+++ marathon/lib/screens/authenticate/cityConfirmation.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:latlong/latlong.dart';
 
 class ConfirmationPage extends StatefulWidget {
   ConfirmationPage();
@@ -10,32 +12,77 @@ class ConfirmationPage extends StatefulWidget {
 class _ConfirmationPageState extends State<ConfirmationPage> {
 
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  final Distance distance = new Distance();
 
   Position _currentPosition;
-  String _currentAddress = "";
+  String _currentAddress = "", _currentCity = "", _currentCityId="";
 
   @override
   Widget build(BuildContext context) {
-    _getCurrentLocation();
     return Scaffold(
         appBar: AppBar(
           title: Text("Confirm City"),
         ),
-        body: Center(
+        body: StreamBuilder(
+          stream: Firestore.instance.collection("cities").snapshots(),
+          builder: (context, snapshots) {
+            _getCurrentLocation();
+            if(_currentAddress!="")
+            {
+              List<DocumentSnapshot> cityList = snapshots.data.documents
+                          .where((DocumentSnapshot documentSnapshot) => (/*(_currentAddress.toLowerCase().contains(documentSnapshot['cityName'].toLowerCase())) && */(distance(new LatLng(_currentPosition.latitude, _currentPosition.longitude), new LatLng(documentSnapshot["geoLocation"].latitude, documentSnapshot["geoLocation"].longitude)) < 150000 ? true : false))).toList();
+              
+              if(cityList.length>0)
+              {
+                  _currentCity=cityList.first["cityName"];
+                  _currentCityId=cityList.first.documentID;
+              }
+            }
+            if (snapshots.hasData) {
+              print(_currentCityId);
+              print(_currentAddress);
+              return Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text("City: " + _currentAddress),
+              children: 
+              _currentCity=="" ?
+              <Widget>[
+                Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: CircularProgressIndicator(),
+              ),
+              ]
+              :
+              <Widget>[
+                Text("City: " + _currentCity),
                 SizedBox(height: 30,),
                 RaisedButton(
                 child: Text("Confirm"),
                 onPressed: (){
 
                 },
-                )
-              ]),
-        ));
+                ),
+                RaisedButton(
+                child: Text("Retry"),
+                onPressed: (){
+                  setState(() {
+                    _currentCity="";
+                    _currentAddress="";
+                  });
+                  _getCurrentLocation();
+                },
+                ),
+              ]
+              ),
+        );
+            } else {
+              return Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),);
   }
    _getCurrentLocation() {
     geolocator
@@ -67,5 +114,4 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
     }
   }
 }
-
 
